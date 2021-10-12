5933e76e1bb69252397bac09cd0d9691c7f2f520
megan.campbell.1988@gmail.com <megan.campbell.1988@gmail.com>
2021-10-12 18:50:38 -0500

    setting up

---
+++ models/Post.js
@@ -0,0 +1,8 @@
+const { Model, DataType } = require('sequalize');
+const sequalize = require('../config/connection');
+
+class Post extends Model {
+    static upvote(body, models){
+        return
+    }
+}
--- server.js
+++ server.js
@@ -7,4 +7,31 @@ const app = express();
 const PORT = process.env.PORT || 3001; 
 
 const sequelize = require();
-const SequalizeStore = require
+const SequalizeStore = require()
+
+const sess = {
+    secret: 'Top Secret ',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true, 
+    store: new SequalizeStore({
+        db: sequelize
+    })
+};
+
+app.use(session(sess));
+
+const helpers = require('.utils/helpers');
+
+const hbs = exphbs.create({ helpers });
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(require('./controllers'))
+
+sequalize.sync({ force: false }).then(()=> {app.listen(PORT, () => console.log('Now listening on ${PORT}'))})
