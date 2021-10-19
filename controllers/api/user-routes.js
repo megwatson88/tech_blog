33c8c8f843c739bd037ed3f0629629e1ef20a552
megan.campbell.1988@gmail.com <megan.campbell.1988@gmail.com>
2021-10-18 21:47:27 -0500

    user-router additions

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -94,3 +94,52 @@ router.post('/', (req, res) => {
     });
 });
 
+router.post('/logout', (req, res) => {
+    if (res.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    }
+    else {
+        res.status(404).end();
+    }
+});
+
+router.put('/:id', (req, res) => {
+    User.update(req.body, {
+        individualHooks: true,
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbUserData => {
+        if (!dbUserData) {
+            res.status(404).json({ message: 'No user found with this id' });
+            return;
+        }
+        res.json(dbUserData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+router.delete('/:id', (req, res) => {
+    User.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbUserData => {
+        if (!dbUserData) {
+            res.status(404).json
+        }
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+module.exports = router; 
