cfad23aea3fb2db03cc7514072059713be87fb37
megan.campbell.1988@gmail.com <megan.campbell.1988@gmail.com>
2021-10-18 21:26:37 -0500

    user route additions

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -46,3 +46,51 @@ router.get('/:id', (req, res) => {
     });
 });
 
+router.post('/', (req, res) =>  {
+    User.create({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password
+    })
+    .then(dbUserData => {
+        req.session.save(()=> {
+         req.session.user_is = dbUserData.id;
+         req.session.username = dbUserData.user_name;
+         req.session.loggedIn = true;
+         
+         res.json(dbUserData);
+        })
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+router.post('/', (req, res) => {
+    User.findOne({
+        where: {
+            email: req.body.email
+        }
+    })
+    .then(dbUserData => {
+        if (!dbUserData) {
+            res.status(400).json({ message: 'No user with that email' });
+            return;
+        }
+        const validPassword = dbUserData.checkPassword(req.body.password);
+
+        if (!validPassword) {
+            res.status(400).json({ message: 'Incorrect password!'});
+            return;
+        }
+        req.session.save(() =>{
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.id;
+            req.session.loggedIn = true;
+
+            res.json({ user: dbUserData, message: 'You are now logged in! '});
+        });
+    });
+});
+
