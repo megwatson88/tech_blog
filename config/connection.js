ef26a433d3d7b96ab44e8b1b7b42cc84aa8ca964
megan.campbell.1988@gmail.com <megan.campbell.1988@gmail.com>
2021-11-02 12:59:44 -0500

    git ignore:

--- config/connection.js
+++ config/connection.js
@@ -7,7 +7,10 @@ const sequelize = process.env.JAWSDB_URL
     : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
         host: 'localhost',
         dialect: 'mysql',
-        port: 3001
+        port: 3306,
+        user: 'root',
+        password: '',
+
 
     });
 
